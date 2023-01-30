c8d3e7e2b37682534828449f440f79f381c057d7
Pericles <r.hoffbuhr@stud.bhh.de>
2023-01-30 11:51:27 +0100

    removed png added exception handling for no db
    Signed-off-by: Pericles <r.hoffbuhr@stud.bhh.de>

--- path.py
+++ path.py
@@ -3,16 +3,18 @@ from flask_mongoengine import MongoEngine
 
 
 app = Flask(__name__)
+try:
+    db = MongoEngine()
+    app.config["MONGODB_SETTINGS"] = [
+        {
+    #        "db": "test",
+            "host": "mongodb+srv://queryqueens:SuperSicher123.@cluster0.r9xszy9.mongodb.net/shop",
+    #        "port": 27017,
+        }
+    ]
 
-db = MongoEngine()
-app.config["MONGODB_SETTINGS"] = [
-    {
-#        "db": "test",
-        "host": "mongodb+srv://queryqueens:SuperSicher123.@cluster0.r9xszy9.mongodb.net/shop",
-#        "port": 27017,
-    }
-]
+    db.init_app(app)
 
-db.init_app(app)
-
-db = MongoEngine(app=app)
+    db = MongoEngine(app=app)
+except Exception as e:
+    print(e)
