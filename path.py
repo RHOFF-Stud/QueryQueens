1376b72d66f59cdf90c7ee28e0e516c59a14d8b4
r.Hoff <r.hoffbuhr@stud.bhh.de>
2023-02-01 15:19:28 +0100

    missing push
    Signed-off-by: r.Hoff <r.hoffbuhr@stud.bhh.de>

--- path.py
+++ path.py
@@ -1,18 +1,23 @@
 from flask import Flask
 from flask_mongoengine import MongoEngine
 from dotenv import load_dotenv
+from flask_cors import CORS
 import os
 
 load_dotenv()
 
 app = Flask(__name__)
+
+CORS(app)
+
 try:
     db = MongoEngine()
     app.config["MONGODB_SETTINGS"] = [
         {
-            "db": os.getenv('DATABASE'),
-            "host": os.getenv('HOST'),
-            "port": int(os.getenv('PORT')),
+            #"db": os.getenv('DATABASE'),
+            #"host": os.getenv('HOST'),
+            "host": "mongodb+srv://queryqueens:SuperSicher123.@cluster0.r9xszy9.mongodb.net/test"
+            #"port": int(os.getenv('PORT')),
             #"user": os.getenv('USER'),
             #"password": os.getenv('PASSWORD')
         }
